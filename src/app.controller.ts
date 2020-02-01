132eb59caf664f2ac8807e83ea57b7ce37e68d87
madisapsalon <madisapsalon@gmail.com>
2020-02-01 14:42:27 +0200

    Add JWT functionality

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './modules/auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
 
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
   async login(@Req() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 
   @Get()
